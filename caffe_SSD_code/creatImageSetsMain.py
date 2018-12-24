673e0caa71b484ed2f875b47baddd9cb743c7760
suiyang <wowchinasuiyang@163.com>
2018-12-24 15:36:55 +0800

    Update creatImageSetsMain.py

--- caffe_SSD_code/creatImageSetsMain.py
+++ caffe_SSD_code/creatImageSetsMain.py
@@ -1,13 +1,32 @@
 # -*- coding:utf-8 -*-
 import os
+import shutil
+import os.path as osp
 
-JPEGImagesPath = '/home/sy/VOT/caffe/Stamp/data/JPEGImages/'
-ImageSets = '/home/sy/VOT/caffe/Stamp/data/ImageSets/Main/'
+imageSourcePath = '/home/lq/fire_detection/fire_pic_change'
+xmlSourcePath = '/home/lq/fire_detection/fire_xml_change'
+
+objdatapath = '../data'
+JPEGImagesPath = osp.join(objdatapath, 'JPEGImages')
+ImageSets_Main_Path = osp.join(objdatapath, 'ImageSets/Main')
+AnnotationsPath = osp.join(objdatapath, 'Annotations')
+
+def generateDataPath(imgsourcepath, xmlsourcepath):
+    for path in [JPEGImagesPath, ImageSets_Main_Path, AnnotationsPath]:
+        if not osp.exists(path):
+            os.makedirs(path)
+    for imgname in os.listdir(imgsourcepath):
+        shutil.copy(osp.join(imgsourcepath, imgname), osp.join(JPEGImagesPath, imgname)) 
+    for xmlname in os.listdir(xmlsourcepath):
+        shutil.copy(osp.join(xmlsourcepath, xmlname), osp.join(AnnotationsPath, xmlname)) 
+
+generateDataPath(imageSourcePath, xmlSourcePath)
+
 trainvalpath = 'trainval.txt'
 testpath = 'test.txt'
 #打开图片列表清单txt文件
-ft = open(ImageSets + trainvalpath,"w")
-fv = open(ImageSets + testpath,"w")
+ft = open(osp.join(ImageSets_Main_Path, trainvalpath),"w")
+fv = open(osp.join(ImageSets_Main_Path, testpath),"w")
 #遍历所有文件名
 val = 0
 lenJPEG = len(os.listdir(JPEGImagesPath))
